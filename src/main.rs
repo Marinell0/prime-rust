ed24352595f8f606ad3d87d498526a3fcb68ecf3
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2023-12-23 11:35:44 -0300

    Calculate primes using sieve of eratosthenes

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,44 @@ use std::io;
 fn main() {
     println!("This is a program to calculate how many prime numbers are there between 1 and N.");
     println!("Please input N:");
-    let n_primes: i32 = read_input();
+    let n_primes: usize = read_input();
 
     // Print input
     println!("Your input: {}", n_primes);
+
+    // Calculate primes
+    let mut primes: Vec<bool> = vec![true; n_primes];
+    primes[0] = false;
+    primes[1] = false;
+
+    for i in 2..n_primes {
+        if primes[i] {
+            let mut j = i * i;
+            while j < n_primes {
+                primes[j] = false;
+                j += i;
+            }
+        }
+    }
+
+    // print all primes without changing line
+    for i in 2..n_primes {
+        if primes[i] {
+            print!("{} ", i);
+        }
+    }
+
+    println!("\n");
+    println!("There are {} prime numbers between 1 and {}.", primes.iter().filter(|&&x| x).count(), n_primes);
+    println!("\n");
 }
 
-fn read_input() -> i32 {
+fn read_input() -> usize {
     let mut input = String::new();
 
     match io::stdin().read_line(&mut input) {
         Ok(_n) => {
-            let is_numeric = input.trim().parse::<i32>();
+            let is_numeric = input.trim().parse::<usize>();
 
             match is_numeric {
                 Ok(n) => {
